7ac859f20172eca498260b132c936f6e5021c00c
Lori <baldomerland@gmail.com>
2022-09-07 09:59:57 +0200

    set search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { text } from 'stream/consumers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Radio Factoria!</h1>
       </header>
+      <section className="search-bar">
+        <input type="text"
+              placeholder="Write station name" />
+              <button name="search">Search</button>
+      </section>
     </div>
   );
 }
